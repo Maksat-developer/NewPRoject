428b1b48bb9a5a0ae28f3bd25116c765a2321da8
Maksat-developer <gangsta.okulus02@gmail.com>
2024-12-28 15:49:39 +0600

    modif/Python/HelloPython/Dictionary/main.py /and/start/new/MovRest2

--- Python/HelloPython/Dictionary/main.py
+++ Python/HelloPython/Dictionary/main.py
@@ -1,3 +1,33 @@
-country = {4: 3}
-print(country[4])
+# userInput = int(input("Enter 1 or 2: "))
+#
+# if userInput == 1:
+#     print("Hello World!")
+#     print("How are you?")
+# elif userInput == 2:
+#     print("Python rocks!")
+#     print("I love Python")
+# else:
+#     print("You did not enter a valid number")
+#
+
+# pets = ['cats', 'dogs', 'rabbits', 'hamsters']
+#
+# for index, myPets in enumerate(pets):
+#     print(index, myPets)
+
+for i in range():
+    print(i)
+
+
+# message = "Hello"
+# for i in message:
+#     print(i)
+
+# age = {'Peter': 5, 'John': 7}
+# for i, j in age.items():
+#     print("Name = %s, Age = %d" %(i, j))
+
+# for i in age:
+#     print(i)
+
 
